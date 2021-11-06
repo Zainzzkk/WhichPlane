41e8890b9dea2a48445b3555e9bc394462272a10
zainzzkk <51957827+Zainzzkk@users.noreply.github.com>
2021-11-06 22:00:19 +0000

    nuke repo

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import locationReducer from '../reducers/LocationReducer';
-
-const rootReducer = combineReducers({
-  locationReducer,
-});
-export const store = createStore(rootReducer, applyMiddleware(thunk));
